9e0b1e8f81ffff029286216482a15eedb1606c25
Vicente Mundim <vicente.mundim@gmail.com>
2013-01-04 18:50:57 -0200

    Some internal refactoring

--- lib/dyno_scaler/manager.rb
+++ lib/dyno_scaler/manager.rb
@@ -15,7 +15,7 @@ module DynoScaler
     def scale_up?
       workers_needed = number_of_workers_needed
 
-      options[:pending] > 0 && workers_needed > options[:workers] && workers_needed <= config.max_workers
+      pending_jobs? && workers_needed > options[:workers] && workers_needed <= config.max_workers
     end
 
     def scale_down(options)
@@ -27,7 +27,7 @@ module DynoScaler
     end
 
     def scale_down?
-      options[:workers] > config.min_workers && options[:pending] == 0 && options[:working] == 0
+      options[:workers] > config.min_workers && !pending_jobs? && !working_jobs?
     end
 
     def scale_with(options)
@@ -40,6 +40,14 @@ module DynoScaler
     protected
       def config
         DynoScaler.configuration
+      end
+
+      def pending_jobs?
+        options[:pending] > 0
+      end
+
+      def working_jobs?
+        options[:working] > 0
       end
 
       def number_of_workers_needed
